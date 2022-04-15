b71c909162e0a2dc755cd8c6cfce134b90d09c6e
t3xtwrit3r <maksudmubin@gmail.com>
2022-04-16 01:49:00 +0600

    project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'usssaiemployees'`, () => {
+  it(`should have as title 'Usssai Employees'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('usssaiemployees');
+    expect(app.title).toEqual('Usssai Employees');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('usssaiemployees app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Usssai Employees app is running!');
   });
 });
---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import {HttpClient} from "@angular/common/http"
+import { Employee } from "./employee";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService{
+
+    private apiServiceUrl = environment.apiBaseUrl;
+
+    constructor(private http: HttpClient) {}
+
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServiceUrl}/employee/all`);
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServiceUrl}/employee/add`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServiceUrl}/employee/update`, employee);
+    }
+
+    public deleteEmployee(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServiceUrl}/employee/delete/${employeeId}`);
+    }
+
+}
